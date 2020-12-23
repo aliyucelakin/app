80f611fd75532fd06aff420d183a86bd2952c362
Ali Yucel <aliyucel.akin@pru.edu.tr>
2020-12-23 13:19:43 +0300

    Updates

---
+++ precache-manifest.05d414818a7549829b80295c3929c035.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83f515a0819773c45fde9a2d5c765813",
+    "url": "/aliyucelakin.github.io/app/index.html"
+  },
+  {
+    "revision": "2c2d207caaa54e11e621",
+    "url": "/aliyucelakin.github.io/app/static/js/2.2ec8f007.chunk.js"
+  },
+  {
+    "revision": "6e1d070858ea330537bcee47e556c855",
+    "url": "/aliyucelakin.github.io/app/static/js/2.2ec8f007.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "088ce362378e27627062",
+    "url": "/aliyucelakin.github.io/app/static/js/main.70a67046.chunk.js"
+  },
+  {
+    "revision": "8bb1b14df2a077dd0981",
+    "url": "/aliyucelakin.github.io/app/static/js/runtime-main.a344d712.js"
+  }
+]);
